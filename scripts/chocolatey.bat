49ec021d7591dda90da01f22cb42c6fc64b2bea3
Manuel Romero <mjromper@gmail.com>
2020-03-02 18:06:43 +0100

    fix choco install

--- scripts/chocolatey.bat
+++ scripts/chocolatey.bat
@@ -1,1 +1,1 @@
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" <NUL
+powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
